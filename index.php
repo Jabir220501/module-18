61c6f1d57f8c3a3115a090b9cfd8b8cd298730e3
Jabir <220501@student.glu.nl>
2024-05-23 14:02:35 +0200

    onboarding frontend finished

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dominos Pizza</title>
+    <link rel="stylesheet" href="./assets/css/onboarding.css">
 </head>
+
 <body>
-    
+    <div class="container">
+        <div class="header">
+            <h1>Welcome to<br>Dominos Pizza</h1>
+            <p>Lorem Ipsum</p>
+        </div>
+        <div class="logo">
+            <img src="./assets/image/dominos-logo.png" alt="Dominos Logo">
+        </div>
+        <div class="buttons">
+            <a href="./menu.php"> <button class="eat-here">Eat here</button></a>
+            <a href="./menu.php"><button class="takeaway">Takeaway</button></a>
+        </div>
+    </div>
 </body>
+
 </html>
